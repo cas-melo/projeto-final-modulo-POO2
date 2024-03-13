4bf31425f2b6b795fe30dcdc3f271e5bf9ebc943
Lucas Melo <casmelo.ms@gmail.com>
2024-03-13 20:32:13 -0300

    criação das classes responsaveis pelo desconto

---
+++ src/models/CalculadoraDesconto.java
@@ -0,0 +1,12 @@
+package models;
+
+public class CalculadoraDesconto implements Desconto {
+
+    @Override
+    public double calcularDesconto(double valorTotal, int duracaoAluguel) {
+
+        // TODO
+        
+        return 0;
+    }
+}
---
+++ src/models/Desconto.java
@@ -0,0 +1,6 @@
+package models;
+
+public interface Desconto {
+
+    double calcularDesconto(double valorTotal, int duracaoAluguel);
+}
