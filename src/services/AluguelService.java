7e7d2038262246277975aa7d61f3293e06a679fc
RenanGroh <renangroh@gmail.com>
2024-03-16 19:54:02 -0300

    aluguel service

--- src/services/AluguelService.java
+++ src/services/AluguelService.java
@@ -1,4 +1,37 @@
 package services;
 
+import models.Aluguel;
+import models.Cliente;
+import models.Veiculo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class AluguelService {
+
+    private Map<Cliente, List<Veiculo>> alugueis;
+
+    public AluguelService() {
+        this.alugueis = new HashMap<>();
+    }
+
+    public void alugarVeiculo(Cliente cliente, Veiculo veiculo) {
+        if (veiculo.isDisponivel()) {
+            List<Veiculo> veiculosAlugados = alugueis.getOrDefault(cliente, new ArrayList<>());
+            veiculosAlugados.add(veiculo);
+            alugueis.put(cliente, veiculosAlugados);
+            veiculo.setDisponivel(false);
+        } else {
+            System.out.println("Veículo não está disponível");
+        }
+    }
+
+    public void devolverVeiculo(Cliente cliente, Veiculo veiculo) {
+        if (alugueis.containsKey(veiculo) && alugueis.get(cliente).contains(veiculo)) {
+            veiculo.setDisponivel(true);
+            alugueis.get(cliente).remove(veiculo);
+        }
+    }
 }
